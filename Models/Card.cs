0b423a3682c0d82a1bb2b3e4e3fb57fb12bff203
ismail assakali <ismail.assakali@student.hogent.be>
2020-10-03 22:13:47 +0200

    make class card

---
+++ Models/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJackGame.Models
+{
+    public class Card
+    {
+        #region Propreties
+        public bool FacuUp { get; set; }
+        public int Value { get; }
+        #endregion
+
+        #region Constructor
+        public Card(Suit suit, FaceValue faceValue)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
