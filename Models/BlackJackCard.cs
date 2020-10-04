35305062c7cf9cf5a298500455d4d1944c2c03e9
ismail assakali <ismail.assakali@student.hogent.be>
2020-10-04 06:04:12 +0200

    made BlackJackCard class

---
+++ Models/BlackJackCard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJackGame.Models
+{
+    public class BlackJackCard : Card
+    {
+
+        #region Propreties
+        public bool FaceUp { get; set; }
+        public int Value { get; }
+        #endregion
+
+        #region Constructor
+        public BlackJackCard(Suit suit, FaceValue faceValue) : base(suit, faceValue)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Methodes
+        public void TurnCard()
+        {
+
+        } 
+        #endregion
+
+    }
+}
