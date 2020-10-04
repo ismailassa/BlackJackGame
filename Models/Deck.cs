7a464a164798dae6f481763150a87855954a23a6
ismail assakali <ismail.assakali@student.hogent.be>
2020-10-04 06:12:14 +0200

    made Deck class

---
+++ Models/Deck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BlackJackGame.Models;
+
+namespace BlackJackGame.Models
+{
+    public class Deck
+    {
+
+        #region Fields
+        private Random _random;
+        #endregion
+
+        public Deck()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public BlackJackCard Draw() 
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TurnCard() 
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
